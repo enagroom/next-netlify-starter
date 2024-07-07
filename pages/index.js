dfa7965fadc91dc8f0c6ac2dea93c9d47f086c92
enagroom <morganeaurillon1@gmail.com>
2024-07-07 22:07:57 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,33 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Message quotidien</title>
+<style>
+  body {
+    font-family: Arial, sans-serif;
+    background-image: url('"C:\Users\morga\Pictures\test.jpg"'); /* Remplacez par le chemin de votre image */
+    background-size: cover; /* Pour s'assurer que l'image couvre toute la zone */
+    background-position: center; /* Positionnez l'image au centre */
+    height: 100vh; /* Hauteur de la page à 100% de la vue */
+    margin: 0; /* Supprime les marges par défaut */
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+  iframe {
+    width: 100%;
+    height: 300px;
+    border: 0;
+    margin: 0;
+    padding: 0;
+  }
+</style>
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
-    </div>
-  )
-}
+</head>
+<body>
+    <iframe src="https://script.google.com/macros/s/AKfycbxT4KmEjyDrBFRgWgN-wObtEYl0-_EZr-_lp-o_Co78WWh-swv_6RtcwrpixkIpu3lxXg/exec" width="100%" height="300" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+</body>
+</html>
